e84cc783e1d9553c6cb53aed2fc03b6e39db51b7
edenpan1207 <xadam69@gmail.com>
2017-03-10 15:03:54 +0800

    final fix layout and text centent

--- js/creative.js
+++ js/creative.js
@@ -2,19 +2,29 @@
     "use strict"; // Start of use strict
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
+
+    $('a.brand-title').click(function(e) {
+        e.preventDefault();
+        $('html,body').stop().animate({ scrollTop: 0 }, 1250, 'easeInOutExpo');
+    });
+
+
     $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        var connect = $(this).attr('href');
+        var position = $(connect).offset().top;
+        $('body').stop().animate({
+            scrollTop: position - 50
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
 
+    
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
         target: '.navbar-fixed-top',
         offset: 51
     });
+
 
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function() {
